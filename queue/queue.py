1f4882dee761c786b8aa75c0b5e86c2e4ccebfe8
Carlos Banks <cbanks@clypr.co>
2020-08-22 15:29:33 -0400

    passing all tests in queue

--- queue/queue.py
+++ queue/queue.py
@@ -25,14 +25,15 @@ class Queue:
         self.storage = LinkedList()
     
     def __len__(self):
-        return len(self.storage)
+        return self.size
 
     def enqueue(self, value):
         self.storage.add_to_tail(value)
         self.size += 1
-        return
 
     def dequeue(self):
-        if len(self.storage) == 0 :
+        if self.size == 0 :
             return None
-        return self.storage.remove_head()
+        else:
+            self.size -= 1
+            return self.storage.remove_head()
